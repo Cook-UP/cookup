ba3397383ef70c3927e0517137c7aa271558a392
Julian Atkin <jmd.atkin@gmail.com>
2021-03-19 02:30:14 +0100

    init

--- server/app.js
+++ server/app.js
@@ -20,7 +20,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+
+app.use('/kitchen', function(req, res, next) {
+  /* MONGODB */
+    res.send("Testing123");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
