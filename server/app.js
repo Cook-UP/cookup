74e303423802792722c999ebde9dd2af39081f2c
Julian Atkin <jmd.atkin@gmail.com>
2021-04-12 22:30:49 +0100

    added cors to app.js

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
 
 //Route objects
@@ -21,6 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors({
+	origin: '*'
+}));
 
 app.use('/', indexRouter);// view engine setup
 app.set('views', path.join(__dirname, 'views'));
