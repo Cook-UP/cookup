0fa5092f89a90882217106b75242ca9bda2d1d36
Israel <israelali142@gamil.com>
2021-05-09 18:11:16 -0400

    Order connection to Datbase made

--- source_folder/client/src/components/Cart/Cart.js
+++ source_folder/client/src/components/Cart/Cart.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { Button } from "../../globalStyles";
-import { Link } from "react-router-dom";
 import CartItem from "./CartItem";
+import { Auth } from "aws-amplify";
+import axios from "axios";
 import {
   GlobalFormStyle,
   StyledFormWrapper,
@@ -17,8 +18,14 @@ import {
 const Cart = ({ cart }) => {
   const [totalPrice, setTotalPrice] = useState(0);
   const [totalItems, setTotalItems] = useState(0);
+  const [user, setUser] = useState(0);
 
   useEffect(() => {
+    // get the Users Info
+    Auth.currentSession()
+      .then((data) => setUser(data.idToken.payload))
+      //.then((data) => console.log(data.idToken.payload.email))
+      .catch((err) => console.log(err));
     /* dynamically Displays the total # of items and total price of the cart */
     let items = 0;
     let price = 0;
@@ -31,6 +38,41 @@ const Cart = ({ cart }) => {
     setTotalItems(items);
     setTotalPrice(price);
   }, [cart, totalPrice, totalItems, setTotalPrice, setTotalItems]);
+
+  const mySubmitHandler = () => {
+    // url to the create order api
+    let Api = "http://3.85.44.54:3000/orders/create";
+    // place the name and qty of each  item in the cart into an array
+    let items = cart.map((item) => {
+      return {
+        name: item.name,
+        quantity: item.qty,
+      };
+    });
+
+    let randomOrderID = parseInt(Math.random() * 10000000);
+
+    // send the order to the Mongobd database
+    axios
+      .post(Api, {
+        oID: randomOrderID,
+        name: user.given_name,
+        address: user.address.formatted,
+        uID: user.sub,
+        kID: "155",
+        items: items,
+        price: totalPrice,
+      })
+      .then(
+        (response) => {
+          console.log("The order has been created");
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
 
   return (
     <div>
@@ -47,12 +89,10 @@ const Cart = ({ cart }) => {
           <h4>Cart Summary</h4>
           <CartDisplayPrice>
             <span>TOTAL: ({totalItems} items)</span>
-            <span>$ { Math.round(totalPrice * 100) / 100}</span>
-             
+            <span>$ {Math.round(totalPrice * 100) / 100}</span>
           </CartDisplayPrice>
-          <Link to="/Checkout">
-            <Button>Proceed To Checkout</Button>
-          </Link>
+
+          {/* <Button onClick={mySubmitHandler}>Proceed To Checkout</Button> */}
         </CartDisplay>
       </StyledFormWrapper>
     </div>
